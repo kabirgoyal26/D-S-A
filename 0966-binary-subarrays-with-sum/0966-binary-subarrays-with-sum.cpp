6dad391b9c31d7ed07237bfe0b23036b81e65a9b
kabirgoyal26 <coodingmy88@gmail.com>
2024-08-28 14:10:52 +0530

    Time: 18 ms (81.56%), Space: 31.4 MB (62.77%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,8 +1,46 @@
 class Solution {
 public:
+
+    int function(vector<int>& nums, int goal)
+    {
+        int l=0;
+        int r=0;
+        int count=0;
+        int sum=0;
+
+        int n= nums.size();
+
+        if(goal<0)
+        {
+            return 0;
+        }
+
+        while(r<n)
+        {
+            sum=sum+nums[r];
+
+            while(sum>goal)
+            {
+                sum=sum-nums[l];
+                l++;
+            }
+
+            count = count + (r-l+1);
+            r++;
+        }
+        return count ;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
+
+        int i= function(nums,goal);
+        int j= function(nums,goal-1);
+
+        return i-j;
+
+
         // esme presum vala concept lag sakta ha 
+        /*
 
         int count=0;
         int presum=0;
@@ -23,6 +61,9 @@ public:
             mpp[presum]= mpp[presum]+1;
         }
         return count ;
+        */
+
+
         
     }
 };
