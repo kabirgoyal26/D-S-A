9403e1c41d38f5df822d639fa86f18426403eadb
kabirgoyal26 <coodingmy88@gmail.com>
2024-09-13 10:42:26 +0530

    Time: 63 ms (50.29%), Space: 42.1 MB (42.84%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -2,23 +2,53 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
 
-    vector<int>ans;
 
+        int n=arr.size();
+        vector<int>store_commulative_xor(n,0);
 
-    for (vector<int>& query : queries )
-    {
-        int left=query[0];
-        int right=query[1];
-        int x=0;
-
-        for (int i = left;i<=right;i++)
+        store_commulative_xor[0]=arr[0];
+        for(int i=1;i<n;i++)
         {
-            x=x^arr[i];
+            store_commulative_xor[i]=store_commulative_xor[i-1]^arr[i];
         }
 
-        ans.push_back(x);
-    }
+        vector<int>ans;
+
+        for (vector<int>& query : queries )
+        {
+            int left=query[0];
+            int right=query[1];
+            
+
+            int m=store_commulative_xor[right]^(left==0?0:store_commulative_xor[left-1]);
+            ans.push_back(m);
+
+            
+        }
     return ans ;
+
+
+
+
+// brute force method 
+//     vector<int>ans;
+//     for (vector<int>& query : queries )
+//     {
+//         int left=query[0];
+//         int right=query[1];
+//         int x=0;
+
+//         for (int i = left;i<=right;i++)
+//         {
+//             x=x^arr[i];
+//         }
+
+//         ans.push_back(x);
+//     }
+//     return ans ;
+
+
+
         
     }
 };
