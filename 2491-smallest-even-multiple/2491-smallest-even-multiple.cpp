0b2f1ad82ad4bfca1df10da5e78436e703ef88d5
kabirgoyal26 <coodingmy88@gmail.com>
2024-09-14 22:38:06 +0530

    Time: 0 ms (100%), Space: 7.6 MB (28.67%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int smallestEvenMultiple(int n) {
 
-        for (int i=2;i<INT_MAX;i=i+2)
-        {
-            if (i%2==0 &&  i%n==0)
+        
+            if (n%2==0 )
             {
-                return i;
+                return n;
             }
-        }
-        return 0;
+        
+        return n*2;
     }
 };
