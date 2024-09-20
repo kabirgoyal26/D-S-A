f5ae07ed000233adae28463f32c8e43f43a9a6fe
kabirgoyal26 <coodingmy88@gmail.com>
2024-09-20 15:13:27 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.66%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -5,41 +5,41 @@ public:
         unordered_map<string,int>mpp;
         string k="";
 
-        // Processing the first sentence s1
+        
         for (int i = 0; i < s1.size(); i++) {
             if (s1[i] == ' ') {
                 mpp[k]++;
-                k = ""; // Reset k to start a new word
+                k = ""; 
             } else {
                 k = k + s1[i];
             }
         }
-        // Add the last word in s1 to the map
+       
         if (!k.empty()) {
             mpp[k]++;
         }
 
-        k = ""; // Reset k for s2
+        k = ""; 
 
-        // Processing the second sentence s2
+        
         for (int i = 0; i < s2.size(); i++) {
             if (s2[i] == ' ') {
                 mpp[k]++;
-                k = ""; // Reset k to start a new word
+                k = ""; 
             } else {
                 k = k + s2[i];
             }
         }
-        // Add the last word in s2 to the map
+        
         if (!k.empty()) {
             mpp[k]++;
         }
 
         vector<string> ans;
 
-        // Find the uncommon words
+        
         for (auto i : mpp) {
-            if (i.second == 1) { // A word is uncommon if it appears only once
+            if (i.second == 1) { 
                 ans.push_back(i.first);
             }
         }
