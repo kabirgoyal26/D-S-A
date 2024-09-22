7f745818e11fab559ff99c273e876b1900d6080c
kabirgoyal26 <coodingmy88@gmail.com>
2024-09-23 00:01:10 +0530

    Time: 3 ms (70.59%), Space: 12.4 MB (6.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,17 +1,40 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+
+//         int i=0;
+
+//         while( i<nums.size() && nums[i]<=target )
+//         {
+//             if (nums[i]==target)
+//             {
+//                 return i;
+//             }
+//             i++;
+//         }
+//         return i;
+//     }
+// };
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-
-        int i=0;
-
-        while( i<nums.size() && nums[i]<=target )
-        {
-            if (nums[i]==target)
-            {
-                return i;
-            }
-            i++;
+        int r=nums.size();
+        int l=0;
+        int m;
+        int ans=-1;
+        if(target>nums[r-1]){
+            return r;
         }
-        return i;
+        while(l<=r){
+            m=l+(r-l)/2;
+            if(nums[m]>=target){
+                ans=m;
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return ans;
     }
 };
