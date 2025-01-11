100d424afa66482441d89ad7ea722ab763798af0
kabirgoyal26 <coodingmy88@gmail.com>
2025-01-11 16:26:41 +0530

    Time: 5 ms (57.69%), Space: 16 MB (24.61%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,30 +1,147 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
 
-        int n= nums.size();
-        long long  sum=0;
+     vector<int>findnsearray(vector<int>& arr1)
+     {
+        int n=arr1.size();
+        vector<int>k(n,n);
+        stack<int>st;
+
+        for (int i=n-1;i>=0;i--)
+        {
+            while (!st.empty() && arr1[st.top()]>=arr1[i])
+            {
+                st.pop();
+            }
+
+            k[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
+        return k;
+
+     }
+     vector<int>findngearray(vector<int>& arr1)
+     {
+        int n=arr1.size();
+        vector<int>k(n,n);
+        stack<int>st;
+
+        for (int i=n-1;i>=0;i--)
+        {
+            while (!st.empty() && arr1[st.top()]<=arr1[i])
+            {
+                st.pop();
+            }
+
+            k[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
+        return k;
+
+     }
+     vector<int>findpsearray(vector<int>& arr1)
+     {
+        int n=arr1.size();
+        vector<int>k(n,-1);
+        stack<int>st;
 
         for (int i=0;i<n;i++)
         {
-            long long max=nums[i];
-            long long min=nums[i];
-
-            for (int j=i;j<n;j++)
+            while (!st.empty() && arr1[st.top()]>arr1[i])
             {
-                if (nums[j]>max)
-                {
-                    max=nums[j];
-                }
-                else if (nums[j]<min)
-                {
-                    min=nums[j];
-                }
-
-                long long  range=max-min;
-                sum=sum+range;
+                st.pop();
             }
+
+            k[i]=st.empty()?-1:st.top();
+            st.push(i);
         }
-       return sum; 
+        return k;
+
+     }
+     vector<int>findpgearray(vector<int>& arr1)
+     {
+        int n=arr1.size();
+        vector<int>k(n,-1);
+        stack<int>st;
+
+        for (int i=0;i<n;i++)
+        {
+            while (!st.empty() && arr1[st.top()]<arr1[i])
+            {
+                st.pop();
+            }
+
+            k[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        return k;
+
+     }
+
+
+
+    long long findsubarraymin(vector<int>& arr)
+    { 
+        long long  ans =0;
+        int n=arr.size();
+
+        vector<int>nsearray=findnsearray(arr);
+        vector<int>psearray=findpsearray(arr);
+
+
+        for (int i=0;i<n;i++)
+        {
+            long long  nse=nsearray[i];
+            long long  pse=psearray[i];
+
+            long long   loc1=nse-i;
+            long long  loc2=i-pse;
+
+            ans = ans + (loc1 * loc2 * arr[i] );
+
+        }
+        return ans ;
+
+
+    }
+
+    long long findsubarraymax(vector<int>& arr)
+    { 
+        long long  ans =0;
+        int n=arr.size();
+
+        vector<int>ngearray=findngearray(arr);
+        vector<int>pgearray=findpgearray(arr);
+
+        for (int i=0;i<n;i++)
+        {
+            long long  nge=ngearray[i];
+            long long  pge=pgearray[i];
+
+            long long   loc1=nge-i;
+            long long  loc2=i-pge;
+
+            ans = ans + (loc1 * loc2 * arr[i] );
+
+        }
+        return ans ;
+
+
+    }
+
+
+    long long subArrayRanges(vector<int>& nums) {
+
+        
+
+        long long  max=findsubarraymax(nums);
+        long long  min=findsubarraymin(nums);
+
+
+        return max-min;
+
+        
+
+        
     }
 };
