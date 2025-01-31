5e5fda56a25b29e2f63412e01b56a1d27c083e7e
kabir goyal <iamkabirgoyal26@gmail.com>
2025-01-31 23:27:49 +0530

    Time: 142 ms (35.31%), Space: 11.2 MB (66.58%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans = 0;
+        
+       
+        for (int i = 0; i < s.size(); i++) {
+            vector<int> store(26, 0);  
+            
+           
+            for (int j = i; j < s.size(); j++) {
+                
+                store[s[j] - 'a']++;
+                
+                
+                int maxFreq = *max_element(store.begin(), store.end());
+                int minFreq = INT_MAX;
+                
+                
+                for (int k = 0; k < 26; k++) {
+                    if (store[k] > 0) {
+                        minFreq = min(minFreq, store[k]);
+                    }
+                }
+                
+               
+                ans += (maxFreq - minFreq);
+            }
+        }
+        
+        return ans;
+    }
+};
