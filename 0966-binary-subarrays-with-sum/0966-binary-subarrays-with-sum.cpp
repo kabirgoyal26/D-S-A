207afe459aff6ae2f98be88724e41a8be38a7f6b
kabir goyal <iamkabirgoyal26@gmail.com>
2025-02-05 21:37:53 +0530

    Time: 0 ms (100%), Space: 32.7 MB (59.89%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,69 +1,36 @@
 class Solution {
 public:
-
     int function(vector<int>& nums, int goal)
     {
         int l=0;
         int r=0;
-        int count=0;
+        int ans =0;
         int sum=0;
 
-        int n= nums.size();
-
-        if(goal<0)
+        while (r<nums.size())
         {
-            return 0;
-        }
+            if (goal<0)
+            {
+                return 0;
+            }
+            sum= sum + nums[r];
 
-        while(r<n)
-        {
-            sum=sum+nums[r];
-
-            while(sum>goal)
+            while (sum>goal)
             {
                 sum=sum-nums[l];
                 l++;
+
             }
 
-            count = count + (r-l+1);
+           ans = ans + (r-l+1);
             r++;
         }
-        return count ;
+        return ans;
+
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-
-        int i= function(nums,goal);
-        int j= function(nums,goal-1);
-
-        return i-j;
-
-
-        // esme presum vala concept lag sakta ha 
-        /*
-
-        int count=0;
-        int presum=0;
-        map<int,int>mpp;
-
-        // mpp[0] =1 jarrori ha 
-        mpp[0]=1;
-
-
-        for(int i=0;i<nums.size();i++)
-        {
-            presum=presum+nums[i];
-
-            int remove = presum-goal;
-
-            count = count + mpp[remove];
-
-            mpp[presum]= mpp[presum]+1;
-        }
-        return count ;
-        */
-
-
+        return function(nums,goal)-function(nums,goal-1);
         
     }
 };
