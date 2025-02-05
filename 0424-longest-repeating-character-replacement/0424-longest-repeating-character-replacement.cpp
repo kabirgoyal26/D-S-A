f2f7ffde5413703af3a8feda4611c41aa88fa4f6
kabir goyal <iamkabirgoyal26@gmail.com>
2025-02-05 20:39:20 +0530

    Time: 3 ms (75.92%), Space: 10.9 MB (72.53%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // onlu one formula is no. of changes is length - maxfrequency
         int l=0;
         int r=0;
         int maxlength=0;
-        int maxfreq=0;
-        // hash ko as vector he karna pdega 
-        vector<int>hash(26,0);
-        
-        while(r<s.size())
+
+        vector<int>vec(26,0);
+
+        while (r<s.size())
         {
-            hash[s[r]-'A']++;
-            maxfreq=max(maxfreq,hash[s[r]-'A']);// ham length same rakh rahe ah just ek eke kar ke bada rag=he ha 
-            if((r-l+1)- maxfreq >k)
+            vec[s[r]-'A']++;
+
+            int maxf=max(maxf,vec[s[r]-'A']);
+
+            if (r-l+1-maxf > k)
             {
-                hash[s[l]-'A']--;
+                vec[s[l]-'A']--;
                 l++;
             }
 
-            if((r-l+1)- maxfreq <=k)
+            if (r-l+1-maxf <= k)
             {
                 maxlength=max(maxlength,r-l+1);
             }
             r++;
         }
         return maxlength;
-        
-        
     }
 };
