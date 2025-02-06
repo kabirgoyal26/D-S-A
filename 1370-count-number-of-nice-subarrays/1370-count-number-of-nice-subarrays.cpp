27a7c88e2a1e6987f788f046e6b09eb1528fb759
kabir goyal <iamkabirgoyal26@gmail.com>
2025-02-06 22:25:52 +0530

    Time: 5 ms (72.78%), Space: 71.2 MB (99.66%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,50 +1,49 @@
 class Solution {
-public:
-// consider odd numbers as 1 \
-// even as 0 
-// sum==k 
-
-    int function(vector<int>& nums, int goal)
+public: 
+    int function(vector<int>& nums, int k)
     {
         int l=0;
         int r=0;
+
         int count=0;
-        int sum=0;
 
-        int n= nums.size();
+        int ans=0 ;
 
-        if(goal<0)
+
+
+        while (r<nums.size())
         {
-            return 0;
-        }
 
-        while(r<n)
-        {
-            sum=sum+(nums[r] % 2);
-
-            while(sum>goal)
+            if (k<0)
             {
-                sum=sum-(nums[l] % 2);
+                return 0;
+            }
+            if (nums[r]%2!=0)
+            {
+                count++;
+            }
+
+            while (count>k)
+            {
+                if (nums[l]%2!=0)
+                {
+                    count--;
+                }
                 l++;
             }
 
-            count = count + (r-l+1);
+            if (count<=k)
+            {
+                ans= ans + (r-l+1);
+            }
+
             r++;
         }
-        return count ;
-    }
+        return ans;
 
+    }
     int numberOfSubarrays(vector<int>& nums, int k) {
 
-        int i= function(nums,k);
-        int j= function(nums,k-1);
-
-        return i-j; 
-        
-
-        
-
-        
-        
+        return function(nums,k)-function(nums,k-1);
     }
 };
