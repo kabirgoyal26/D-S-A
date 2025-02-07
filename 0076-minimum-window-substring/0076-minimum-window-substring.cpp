9dcd8d9396cc07a1f46f48a57b38096f77723b90
kabir goyal <iamkabirgoyal26@gmail.com>
2025-02-08 00:32:41 +0530

    Time: 0 ms (100%), Space: 11.4 MB (81.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,51 +1,27 @@
 class Solution {
-public:
-    string minWindow(string s, string t) {
-         int l = 0;
-        int r = 0;
+ public:
+  string minWindow(string s, string t) {
+    vector<int> count(128);
+    int required = t.length();
+    int bestLeft = -1;
+    int minLength = s.length() + 1;
 
-        // min length declaration 
-        int minlength = INT_MAX;
-        int count = 0;
+    for (const char c : t)
+      ++count[c];
 
-        int m = s.size();
-        int n = t.size();
-
-        // making a hash map for character frequencies
-        map<char, int> mpp;
-
-        int startingindex = -1;
-
-        // populate the hash map with frequencies of characters in t
-        for (int i = 0; i < n; i++) {
-            mpp[t[i]]++;
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      if (--count[s[r]] >= 0)
+        --required;
+      while (required == 0) {
+        if (r - l + 1 < minLength) {
+          bestLeft = l;
+          minLength = r - l + 1;
         }
-
-        while (r < m) {
-            if (mpp[s[r]] > 0) {
-                count++;
-            }
-            // ye to har element ke liye hoga 
-            mpp[s[r]]--;
-
-            while (count == n) {
-                if (r - l + 1 < minlength) {
-                    minlength = r - l + 1;
-                    startingindex = l;
-                }
-
-                // reduce the window
-                mpp[s[l]]++;
-
-                if (mpp[s[l]] > 0) {
-                    count--;
-                }
-                l++;
-            }
-            r++;
-        }
-
-        // Return the minimum window substring or an empty string if no valid window was found
-        return (startingindex == -1) ? "" : s.substr(startingindex, minlength);
+        if (++count[s[l++]] > 0)
+          ++required;
+      }
     }
+
+    return bestLeft == -1 ? "" : s.substr(bestLeft, minLength);
+  }
 };
