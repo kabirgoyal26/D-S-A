d4b6ec6c04ff44501f3b2acfda795e1e5b674555
kabir goyal <iamkabirgoyal26@gmail.com>
2025-02-07 23:41:13 +0530

    Time: 94 ms (36.41%), Space: 56.2 MB (64.46%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -4,44 +4,40 @@ public:
     {
         int l=0;
         int r=0;
-        int count =0;
 
-        if(k==0)
-        {
-            return 0;
-        }
+        int ans=0;
 
-        map<int ,int>mpp;
+        unordered_map<int,int>mpp;
 
         while(r<nums.size())
         {
             mpp[nums[r]]++;
 
-            while(mpp.size()>k)
+            while (mpp.size()>k)
             {
                 mpp[nums[l]]--;
 
-                if(mpp[nums[l]]==0)
+                if (mpp[nums[l]]==0)
                 {
                     mpp.erase(nums[l]);
                 }
                 l++;
             }
 
-            count=count+(r-l+1);
+            
+            ans = ans + r-l+1;
             r++;
         }
-        return count;
+        return ans ;
     }
-
-
     int subarraysWithKDistinct(vector<int>& nums, int k) {
 
-        int i=function(nums,k);
-        int j=function(nums,k-1);
 
-        return (i-j);
         
-        
+
+        return function(nums,k)-function(nums,k-1);
+
+       
+
     }
 };
