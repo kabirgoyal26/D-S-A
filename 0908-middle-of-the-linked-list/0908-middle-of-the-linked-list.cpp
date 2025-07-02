460b8fcd419b802a3a26962a6b2340308f1f829a
kabir goyal <iamkabirgoyal26@gmail.com>
2025-07-02 11:03:46 +0530

    Time: 0 ms (100%), Space: 10.1 MB (2.28%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -16,13 +16,13 @@ public:
         ListNode* slow = head ;
 
 
-        while ( fast->next!=nullptr && fast->next->next!=nullptr )
+        while ( fast!=nullptr && fast->next!=nullptr )
         {
             slow=slow->next;
             fast=fast->next->next;
         }
 
-        return fast->next==nullptr?slow:slow->next ;
+        return slow ;
 
 
         
