17b15f3a59d08db8f3e7218e6ce744191a583fdb
kabir goyal <iamkabirgoyal26@gmail.com>
2025-07-04 11:07:17 +0530

    Time: 3 ms (52.15%), Space: 9.9 MB (26.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -7,18 +7,43 @@ public:
         {
             return false ;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+
+        // for (int i=0;i<s.size();i++)
+        // {
+        //     if (s[i]!=t[i])
+        //     {
+        //         return false ;
+        //     }
+        // }
+
+        // return true;
+
+
+        unordered_map<char,int>mpp;
 
         for (int i=0;i<s.size();i++)
         {
-            if (s[i]!=t[i])
+            mpp[s[i]]++;
+        }
+
+        for (int j=0;j<t.size();j++)
+        {
+            if (mpp.find(t[j])!=mpp.end())
+            {
+                mpp[t[j]]--;
+                if (mpp[t[j]]==0)
+                {
+                    mpp.erase(t[j]);
+                }
+            }
+            else
             {
                 return false ;
             }
         }
-
-        return true;
+        return true ;
 
     }
 };
